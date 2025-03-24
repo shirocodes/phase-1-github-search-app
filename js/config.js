42ade0f19e7ad9af4a2b138752a63144163b7a81
shirocodes <ishirapt@gmail.com>
2025-03-24 18:44:37 +0300

    Added necessary files

--- js/config.js
+++ js/config.js
@@ -1,3 +1,5 @@
 //storing API configurations
 const API_KEY = "your_actual_api_key_here";
 const API_URL = "https://api.thedogapi.com/v1";
+
+export {API_KEY, API_URL};
---
+++ js/index.js
@@ -0,0 +1,7 @@
+//API configurations
+import {API_KEY, API_URL} from './config.js';
+
+console.log("API Key Loaded:", API_KEY ? " Successful" : "❌ Failed");
+console.log("API Base URL:", API_URL);
+
+
